14512335e5c2a7ac3087ded290e8506fa115458a
Stuart Yamartino <stuart@yamartino.com>
2016-01-22 20:54:24 -0500

    First push, still needs to be calibrated

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var save = false;
+var saveZero = 0;
+
+Pressure.set('.force-container', {
+  change: function(force, event){
+    if(save){
+      saveZero = force;
+      save = false;
+    }
+    document.getElementById('weight').innerHTML = Pressure.map(force - saveZero, 0, 1, 0, 300).toFixed(2) + 'g';
+  },
+  end: function(){
+    document.getElementById('weight').innerHTML = '0.00g';
+  }
+});
+
+$(function(){
+  $('.zero').click(function(){
+    save = true;
+  });
+});
